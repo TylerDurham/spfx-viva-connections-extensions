a294b139dfb0478acb7320b4fe6622f22904ef0c
Tyler Durham <TylerDurham@users.noreply.github.com>
2022-04-23 02:26:29 -0500

    Added DIVs in portal container Removed redundant code.

--- src/extensions/components/home-button.tsx
+++ src/extensions/components/home-button.tsx
@@ -2,14 +2,13 @@ import * as React from 'react';
 import styles from './home-button.module.scss';
 import { IconButton, IIconProps } from 'office-ui-fabric-react';
 import { IHomeButtonProps } from './home-button.interfaces';
-import { IPortalContext } from '../../common/portal-context.interfaces';
 import { Log } from '../../common/shared-lib';
 import { PortalContext } from '../../common/portal-context';
 
 const icon: IIconProps = {iconName: "Home"};
 
 export const HomeButton = (props: IHomeButtonProps) => {
-
+	
 	const { properties, debug } = React.useContext(PortalContext);
 	const { debugParameters } = debug;
 	let title: string, url: string;
