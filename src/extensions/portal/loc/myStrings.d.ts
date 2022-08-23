2d2aa321ea1564264530bd45cd5a80a5b8668627
Tyler Durham <TylerDurham@users.noreply.github.com>
2022-08-23 11:17:33 -0500

    scaffolding.

---
+++ src/extensions/portal/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface IPortalApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'PortalApplicationCustomizerStrings' {
+  const strings: IPortalApplicationCustomizerStrings;
+  export = strings;
+}
