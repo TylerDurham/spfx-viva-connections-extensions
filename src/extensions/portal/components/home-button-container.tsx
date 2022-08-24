06f99903c8e15c9ec72067664a453cebc00998d8
Tyler Durham <TylerDurham@users.noreply.github.com>
2022-08-24 10:04:56 -0500

    Now handles SPFX debug state.

--- src/extensions/portal/components/home-button-container.tsx
+++ src/extensions/portal/components/home-button-container.tsx
@@ -5,33 +5,38 @@ import { IconButton, IIconProps } from 'office-ui-fabric-react';
 import { PortalContext } from '../../../common/portal-context';
 
 /**
- * Handles the click() event of the IconButton. Redirects the browser to
- * the URL specified in the arguments.
- * @param url The URL to launch.
- */
-const handleOnClick = (url: string): void => {
-    location.href = url;
-};
-
-/**
  * A React component that renders the "Home" button.
  * @param props Configuration properties for the component.
  * @returns The React component.
  */
 export default function HomeButtonContainer(props: IHomeButtonProps): React.ReactElement {
     
-    const { homeSite } = React.useContext(PortalContext);
+    // Grab current context from React
+    const { homeSite, debug } = React.useContext(PortalContext);
     
+    // Configure Icon Button
     const iconProps: IIconProps = {
         iconName: "Home",
         title: `Return to ${homeSite.title}`
     };
 
+    // Handle Icon Button onClick
+    const handleOnClick = () => {
+        let url = `${homeSite.url}`;
+        if (debug.isDebugging) {
+            // Append SPFX debug state to URL.
+            const { debugManifestsFile, customActions, loadSPFX } = debug;
+            url = url + `?debugManifestsFile=${encodeURIComponent(debugManifestsFile)}&loadSPFX=${loadSPFX}&customActions=${customActions}`;
+        }
+
+        location.href = url;
+    }
+
     return (
         <div className={styles.default.homeButtonContainer}>
             <IconButton 
                 iconProps={iconProps} 
-                onClick={() => handleOnClick(homeSite.url)}>
+                onClick={() => handleOnClick()}>
                 
             </IconButton>
         </div>
