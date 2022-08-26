c9cf9a68b9fda488e74880e0f536194f82d53498
Tyler Durham <TylerDurham@users.noreply.github.com>
2022-08-26 11:49:10 -0500

    CLEAR

--- src/extensions/loc/myStrings.d.ts
+++
@@ -1,8 +0,0 @@
-declare interface IPortalApplicationCustomizerStrings {
-  searchPortalNotConfigured: string;
-}
-
-declare module 'PortalApplicationCustomizerStrings' {
-  const strings: IPortalApplicationCustomizerStrings;
-  export = strings;
-}
--- src/webparts/searchConfiguration/components/ISearchConfigurationProps.ts
+++
@@ -1,7 +0,0 @@
-export interface ISearchConfigurationProps {
-  description: string;
-  isDarkTheme: boolean;
-  environmentMessage: string;
-  hasTeamsContext: boolean;
-  userDisplayName: string;
-}
